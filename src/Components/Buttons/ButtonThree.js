2eb6b4f85970592fe4277eaab3cc22dba239d799
daralnoble939 <47654385+daralnoble939@users.noreply.github.com>
2019-03-18 12:55:33 +0530

    Add files via upload

---
+++ src/Components/Buttons/ButtonThree.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ButtonThree.css"
+
+class ButtonThree extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="button">
+                    <p>Download</p>
+                </div>
+            </React.Fragment>
+        );
+
+    }
+}
+
+export default ButtonThree;
