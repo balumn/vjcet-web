c94ac8633adcec33817d41875dfb27467c93591e
balumn <balumn>
2019-03-18 13:31:02 +0530

    Updates

---
+++ precache-manifest.5d3b241a9e19eec3680a4992ae7ae94d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2be2130c4874c357721",
+    "url": "/VJCET/static/css/main.c8252514.chunk.css"
+  },
+  {
+    "revision": "e2be2130c4874c357721",
+    "url": "/VJCET/static/js/main.bb6b1a9c.chunk.js"
+  },
+  {
+    "revision": "6dffb4e2d2d59656712d",
+    "url": "/VJCET/static/js/runtime~main.7df79967.js"
+  },
+  {
+    "revision": "62e454ecaa1dd03e6552",
+    "url": "/VJCET/static/js/2.24334542.chunk.js"
+  },
+  {
+    "revision": "32788e90cf496cf72e4951e2161e4c77",
+    "url": "/VJCET/static/media/right-arrow.32788e90.svg"
+  },
+  {
+    "revision": "952aa590133a9616bf36eba0546d03c1",
+    "url": "/VJCET/static/media/Capture.952aa590.png"
+  },
+  {
+    "revision": "7df6e3f67f9b3c736c25f6ad709dc81d",
+    "url": "/VJCET/static/media/CarouselThreeNext.7df6e3f6.svg"
+  },
+  {
+    "revision": "58bad2a8ca95deec7dfd0053e008f0f4",
+    "url": "/VJCET/static/media/CarouselThreePrevious.58bad2a8.svg"
+  },
+  {
+    "revision": "2640362342464177bf556c8c825a15f3",
+    "url": "/VJCET/static/media/FacilitiesThree.26403623.svg"
+  },
+  {
+    "revision": "053bb75789ac9d6835f76811fe2182e9",
+    "url": "/VJCET/static/media/OPEN.053bb757.svg"
+  },
+  {
+    "revision": "ff86799f23bfc415805cd0a98c47731f",
+    "url": "/VJCET/static/media/VJCLogo.ff86799f.png"
+  },
+  {
+    "revision": "b05e1519f63aa661fb72da3c0fa642be",
+    "url": "/VJCET/static/media/fb.b05e1519.svg"
+  },
+  {
+    "revision": "e9ee7c98194a7d23b5219aa103e288d5",
+    "url": "/VJCET/static/media/hamburger.e9ee7c98.svg"
+  },
+  {
+    "revision": "2dad930e6e0a7ff21883745b493447f5",
+    "url": "/VJCET/static/media/insta.2dad930e.svg"
+  },
+  {
+    "revision": "2cc6e905d64bed8710b98ef9f5bf8811",
+    "url": "/VJCET/static/media/nextArrow.2cc6e905.svg"
+  },
+  {
+    "revision": "4cf5b68e86617ce3257f9335b643cf83",
+    "url": "/VJCET/static/media/previousArrow.4cf5b68e.svg"
+  },
+  {
+    "revision": "230ed48b6b0d5efd84e6cec9ef3870d4",
+    "url": "/VJCET/static/media/stock1.230ed48b.png"
+  },
+  {
+    "revision": "5d1cfa17136984d228811004daebb03b",
+    "url": "/VJCET/static/media/stock2.5d1cfa17.png"
+  },
+  {
+    "revision": "4b95163448ea147f9dd717fb1b74617d",
+    "url": "/VJCET/static/media/stock3.4b951634.png"
+  },
+  {
+    "revision": "638bab1934ce213cc0a4fd2c9dbd24e9",
+    "url": "/VJCET/static/media/twitter.638bab19.svg"
+  },
+  {
+    "revision": "f1fd7e43275c90045c70e93c3e8c18ce",
+    "url": "/VJCET/static/media/vjcbg.f1fd7e43.png"
+  },
+  {
+    "revision": "6d8c54c83942222dfdcd48b582b3f4e1",
+    "url": "/VJCET/static/media/vjcetbg.6d8c54c8.jpg"
+  },
+  {
+    "revision": "6170946f0c9bb047b6b2d2cb585d40c2",
+    "url": "/VJCET/static/media/youtube.6170946f.svg"
+  },
+  {
+    "revision": "2459d9e225d7f723dd5051f7135c8e21",
+    "url": "/VJCET/index.html"
+  }
+];
