c730a8a640738720dbe62ab876be8717e2b84258
Balu M N <balumn@ieee.org>
2019-04-17 01:42:37 +0530

    hostin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from 'react-router-dom'
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
