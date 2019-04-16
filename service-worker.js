86d0525ddd0c7fef1b51e810b6a7e8202b10b370
Balu M N <balumn@ieee.org>
2019-04-17 01:36:19 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vjcet-web/precache-manifest.48cd3a28a6b61c3b3a2ac2b0c7078c0a.js"
+  "/vjcet-web/precache-manifest.c359c5707b5d534af22788fc87ef2ebe.js"
 );
 
 workbox.clientsClaim();
