fb726531b5faf0184590a0234cb8b8cf457bebbb
Balu M N <balumn@ieee.org>
2019-04-16 12:32:56 +0530

    hosting

--- src/AboutPageContainers/AboutVJC/index.js
+++ src/AboutPageContainers/AboutVJC/index.js
@@ -3,6 +3,7 @@ import HeadingTwo from "../../Components/Texts/HeadingTwo";
 import ImageOne from "../../Components/Images/ImageOne";
 import "./About.css";
 import BodyOneJustified from "../../Components/Texts/BodyOneJustified";
+import AcademicsList from "../../Pages/AcademicsContainers/AcademicsList";
 
 class AboutVJC extends React.Component {
   AboutVjcTitle = {
@@ -30,55 +31,6 @@ class AboutVJC extends React.Component {
     title:
       "The building complex of the College is  situated in a sprawling campus of 26 acres, nestled among lush greenery over a hillock on the side of the Ernakulam - Thodupuzha State Highway.  The campus is divided into various functional zones-Main College Building namely; Block A,B,C & D, Gents and Ladies Hostels. The college has spacious playgrounds, synthetic basketball court and gymnasium. ATM counters of South Bank of India and South Indian Bank are there in front of the college gate."
   };
-  PGCourse = {
-    instanceID: "PGCourse",
-    title: "PG Programmes offered:"
-  }
-  PGCourse1 = {
-    instanceID: "PGCourse1",
-    title: "Computer Science & Engineering"
-  }
-  PGCourse2 = {
-      instanceID: "PGCourse2",
-      title: "Electronics and Communication Engineering with specialization VLSI & Embedded Systems."
-  }
-  PGCourse3 = {
-      instanceID: "PGCourse3",
-      title: "Industrial Engineering and Management"
-  }
-  PGCourse4 = {
-      instanceID: "PGCourse4",
-      title: "MBA"
-  }
-  UGCourse = {
-    instanceID: "UGCourse",
-    title: "B-Tech Programmes offered:"
-  }
-  UGCourse1 = {
-      instanceID: "UGCourse1",
-      title: "Civil Engineering"
-  }
-  UGCourse2 = {
-      instanceID: "UGCourse2",
-      title: "Computer Science and Engineering"
-  }
-  UGCourse3 = {
-      instanceID: "UGCourse3",
-      title: "Electronics and Communication engineering"
-  }
-  UGCourse4 = {
-      instanceID: "UGCourse4",
-      title: "Electrical and Electronics Engineering"
-  }
-  UGCourse5 = {
-    instanceID: "UGCourse5",
-    title: "Information Technology"
-  }
-  UGCourse6 = {
-    instanceID: "UGCourse6",
-    title: "Mechanical Engineering"
-  }
-
 
   render() {
     return (
@@ -88,23 +40,7 @@ class AboutVJC extends React.Component {
         <div className="about-para">
           <BodyOneJustified data={this.AboutVjcText1} />
           <p><BodyOneJustified data={this.AboutVjcText2} /></p>
-          <div className="inner-para">
-            <p><BodyOneJustified data={this.PGCourse} /></p>
-            <ul>
-              <li><p><BodyOneJustified data={this.PGCourse1} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse2} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse3} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse4} /></p></li>
-            </ul>
-            <p><BodyOneJustified data={this.UGCourse} /></p>
-            <ul>
-              <li><p><BodyOneJustified data={this.UGCourse1} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse2} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse3} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse4} /></p></li>
-            </ul>
-
-          </div>
+          <AcademicsList />
           <p><BodyOneJustified data={this.AboutVjcText3} /></p>
         </div>
         {/* <ResourcesAvailable /> */}
---
+++ src/Pages/AcademicsContainers/AcademicsList.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import "./HomePage.css";
+import "./AboutPage.css"
+import HeadingTwo from "../../Components/Texts/HeadingTwo";
+import BodyOneJustified from "../../Components/Texts/BodyOneJustified";
+
+class AcademicsList extends Component {
+  state = { showMenu: false };
+
+  openMenu = () => {
+    console.log("opening Menu");
+    this.setState({ showMenu: true });
+  };
+  closeMenu = () => {
+    this.setState({ showMenu: false });
+  };
+  HomePageHeader = {
+    instanceID: "HomePageHeader"
+  };
+  HomePageFooter = {
+    instanceID: "HomePageFooter"
+  };
+
+  AcademicsTitle = {
+    instanceID: "AcademicsTitle",
+    title: "Academics"
+  }
+  AcademicsText = {
+    instanceID: "AcademicsText",
+    title: "Viswajyothi College of Engineering & Technology has 6 academic departments which offer four post graduate and six UnderGraduate Engineering Degree Programs. "
+  }
+
+  PGCourse = {
+    instanceID: "PGCourse",
+    title: "PG Programmes offered:"
+  }
+  PGCourse1 = {
+    instanceID: "PGCourse1",
+    title: "Computer Science & Engineering"
+  }
+  PGCourse2 = {
+    instanceID: "PGCourse2",
+    title: "Electronics and Communication Engineering with specialization VLSI & Embedded Systems."
+  }
+  PGCourse3 = {
+    instanceID: "PGCourse3",
+    title: "Industrial Engineering and Management"
+  }
+  PGCourse4 = {
+    instanceID: "PGCourse4",
+    title: "MBA"
+  }
+  UGCourse = {
+    instanceID: "UGCourse",
+    title: "B-Tech Programmes offered:"
+  }
+  UGCourse1 = {
+    instanceID: "UGCourse1",
+    title: "Civil Engineering"
+  }
+  UGCourse2 = {
+    instanceID: "UGCourse2",
+    title: "Computer Science and Engineering"
+  }
+  UGCourse3 = {
+    instanceID: "UGCourse3",
+    title: "Electronics and Communication engineering"
+  }
+  UGCourse4 = {
+    instanceID: "UGCourse4",
+    title: "Electrical and Electronics Engineering"
+  }
+  UGCourse5 = {
+    instanceID: "UGCourse5",
+    title: "Information Technology"
+  }
+  UGCourse6 = {
+    instanceID: "UGCourse6",
+    title: "Mechanical Engineering"
+  }
+
+  render() {
+    return (
+      <div className="HomePage">
+
+        <div className="AboutPage">
+          <HeadingTwo data={this.AcademicsTitle} /><br />
+          <BodyOneJustified data={this.AcademicsTitle} />
+          <div className="inner-para">
+            <p><BodyOneJustified data={this.PGCourse} /></p>
+            <ul>
+              <li><p><BodyOneJustified data={this.PGCourse1} /></p></li>
+              <li><p><BodyOneJustified data={this.PGCourse2} /></p></li>
+              <li><p><BodyOneJustified data={this.PGCourse3} /></p></li>
+              <li><p><BodyOneJustified data={this.PGCourse4} /></p></li>
+            </ul>
+            <p><BodyOneJustified data={this.UGCourse} /></p>
+            <ul>
+              <li><p><BodyOneJustified data={this.UGCourse1} /></p></li>
+              <li><p><BodyOneJustified data={this.UGCourse2} /></p></li>
+              <li><p><BodyOneJustified data={this.UGCourse3} /></p></li>
+              <li><p><BodyOneJustified data={this.UGCourse4} /></p></li>
+            </ul>
+
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+export default AcademicsList;
