6c01424694e8508b714d733adf1eedb59249874d
Aswin CJ <aswincj4@gmail.com>
2019-04-18 00:12:58 +0530

    Resources Available Component Added to About Page

--- src/AboutPageContainers/AboutVJC/index.js
+++ src/AboutPageContainers/AboutVJC/index.js
@@ -3,6 +3,7 @@ import HeadingTwo from "../../Components/Texts/HeadingTwo";
 import ImageOne from "../../Components/Images/ImageOne";
 import "./About.css";
 import BodyOneJustified from "../../Components/Texts/BodyOneJustified";
+import ResourcesGroup from "../ResourcesGroup/ResourcesGroup";
 
 class AboutVJC extends React.Component {
   AboutVjcTitle = {
@@ -33,52 +34,52 @@ class AboutVJC extends React.Component {
   PGCourse = {
     instanceID: "PGCourse",
     title: "PG Programmes offered:"
-  }
+  };
   PGCourse1 = {
     instanceID: "PGCourse1",
     title: "Computer Science & Engineering"
-  }
+  };
   PGCourse2 = {
-      instanceID: "PGCourse2",
-      title: "Electronics and Communication Engineering with specialization VLSI & Embedded Systems."
-  }
+    instanceID: "PGCourse2",
+    title:
+      "Electronics and Communication Engineering with specialization VLSI & Embedded Systems."
+  };
   PGCourse3 = {
-      instanceID: "PGCourse3",
-      title: "Industrial Engineering and Management"
-  }
+    instanceID: "PGCourse3",
+    title: "Industrial Engineering and Management"
+  };
   PGCourse4 = {
-      instanceID: "PGCourse4",
-      title: "MBA"
-  }
+    instanceID: "PGCourse4",
+    title: "MBA"
+  };
   UGCourse = {
     instanceID: "UGCourse",
     title: "B-Tech Programmes offered:"
-  }
+  };
   UGCourse1 = {
-      instanceID: "UGCourse1",
-      title: "Civil Engineering"
-  }
+    instanceID: "UGCourse1",
+    title: "Civil Engineering"
+  };
   UGCourse2 = {
-      instanceID: "UGCourse2",
-      title: "Computer Science and Engineering"
-  }
+    instanceID: "UGCourse2",
+    title: "Computer Science and Engineering"
+  };
   UGCourse3 = {
-      instanceID: "UGCourse3",
-      title: "Electronics and Communication engineering"
-  }
+    instanceID: "UGCourse3",
+    title: "Electronics and Communication engineering"
+  };
   UGCourse4 = {
-      instanceID: "UGCourse4",
-      title: "Electrical and Electronics Engineering"
-  }
+    instanceID: "UGCourse4",
+    title: "Electrical and Electronics Engineering"
+  };
   UGCourse5 = {
     instanceID: "UGCourse5",
     title: "Information Technology"
-  }
+  };
   UGCourse6 = {
     instanceID: "UGCourse6",
     title: "Mechanical Engineering"
-  }
-
+  };
 
   render() {
     return (
@@ -87,28 +88,67 @@ class AboutVJC extends React.Component {
         <ImageOne data={this.AboutVjcImage} />
         <div className="about-para">
           <BodyOneJustified data={this.AboutVjcText1} />
-          <p><BodyOneJustified data={this.AboutVjcText2} /></p>
+          <p>
+            <BodyOneJustified data={this.AboutVjcText2} />
+          </p>
           <div className="inner-para">
-            <p><BodyOneJustified data={this.PGCourse} /></p>
+            <p>
+              <BodyOneJustified data={this.PGCourse} />
+            </p>
             <ul>
-              <li><p><BodyOneJustified data={this.PGCourse1} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse2} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse3} /></p></li>
-              <li><p><BodyOneJustified data={this.PGCourse4} /></p></li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.PGCourse1} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.PGCourse2} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.PGCourse3} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.PGCourse4} />
+                </p>
+              </li>
             </ul>
-            <p><BodyOneJustified data={this.UGCourse} /></p>
+            <p>
+              <BodyOneJustified data={this.UGCourse} />
+            </p>
             <ul>
-              <li><p><BodyOneJustified data={this.UGCourse1} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse2} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse3} /></p></li>
-              <li><p><BodyOneJustified data={this.UGCourse4} /></p></li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.UGCourse1} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.UGCourse2} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.UGCourse3} />
+                </p>
+              </li>
+              <li>
+                <p>
+                  <BodyOneJustified data={this.UGCourse4} />
+                </p>
+              </li>
             </ul>
-
           </div>
-          <p><BodyOneJustified data={this.AboutVjcText3} /></p>
+          <p>
+            <BodyOneJustified data={this.AboutVjcText3} />
+          </p>
         </div>
-        {/* <ResourcesAvailable /> */}
-      
+
+        <ResourcesGroup />
       </React.Fragment>
     );
   }
