dd6e61ff84b85a5eb0fd614629c714d322898af5
balumn <balumn@ieee.org>
2019-05-15 21:34:23 +0530

    hosting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import HomePage from "./Pages/HomePage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AboutPage from "./Pages/AboutPage";
 import DepartmentPage from "./Pages/Departmentpage";
 import CampusLifePage from "./Pages/CampusLifePage";
